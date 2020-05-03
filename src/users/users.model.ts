ff7f5880ef940dcdb9cbb6f27b2b2414e1e26641
Bernardo Laing <A01206492@itesm.mx>
2020-05-03 09:53:25 -0500

    Fix weird issue where could not run

--- src/users/users.model.ts
+++ src/users/users.model.ts
@@ -1,25 +1,34 @@
 import { ObjectType, Field, ID } from 'type-graphql';
+import { Entity, BaseEntity, PrimaryGeneratedColumn, Column } from 'typeorm';
 
 @ObjectType()
-export class User {
+@Entity()
+export class User extends BaseEntity {
   @Field(() => ID)
+  @PrimaryGeneratedColumn()
   public id: string;
 
   @Field()
+  @Column()
   public firstName: string;
 
   @Field({ nullable: true })
+  @Column()
   public lastName?: string;
 
-  @Field()
-  public profilePicture: string;
+  @Field({ nullable: true })
+  @Column({ nullable: true })
+  public profilePicture?: string;
 
   @Field()
+  @Column()
   public email: string;
 
   @Field()
+  @Column()
   public active: boolean;
 
   @Field()
+  @Column()
   public creationDate: Date;
 }
